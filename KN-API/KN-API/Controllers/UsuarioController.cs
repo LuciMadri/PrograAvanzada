d8feaa18139914c993f17a421aceee22098e150c
LuciMadri <89278285+LuciMadri@users.noreply.github.com>
2022-09-20 21:15:30 -0600

    Semana 02
    Introduccion API´s

---
+++ KN-API/KN-API/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KN_API.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+
+        [HttpGet]
+        [Route("api/Validar_Usuario")]
+
+        public bool Validar_Usuario()
+        {
+            return false;
+        }
+
+        //Registrar Usuario
+        //Validar Usuario
+        //Recuperar password
+        //Cambiar password
+        //Validar permisos de usuario
+    }
+}
---
+++ KN-API/KN-API/Models/UsuarioObj.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KN_API.Models
+{
+    public class UsuarioObj
+    {
+
+        //Atributos o Propiedades de una clase
+
+        public long ConsecutivoUsuario { get; set; }
+
+        public string Correo { get; set; }
+
+        public string password { get; set; }
+
+        public string NombreCompleto { get; set; }
+
+        public int TipoUsuario { get; set; }
+
+        public int Estado { get; set; }
+
+        //ID ---> cedula
+        
+
+    }
+}
