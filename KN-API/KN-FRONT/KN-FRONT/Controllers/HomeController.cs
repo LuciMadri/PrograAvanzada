89cfaf14234488d6f6a8393c121a3549af839105
LuciMadri <89278285+LuciMadri@users.noreply.github.com>
2022-10-25 18:05:32 -0600

    semana 06 frontend

---
+++ KN-API/KN-FRONT/KN-FRONT/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KN_FRONT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Principal()
+        {
+            /*LLAMAR AL API PARA VALIDAR LOS CREDENCIALES*/
+
+            return View();
+        }
+
+    }
+}
